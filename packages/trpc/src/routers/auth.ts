b1b9b34c8fe1100f39b4780ab4ef96fe72babea1
marcotheo <marcobutalid989@gmail.com>
2025-07-02 23:51:46 +0800

    added signIn trpc route

--- packages/trpc/src/routers/auth.ts
+++ packages/trpc/src/routers/auth.ts
@@ -1,10 +1,84 @@
-// import { DailyVerse } from "@daily-walk/core/daily-verse";
+import { z } from "zod";
+import dayjs from "dayjs";
+
+import { signInUser } from "@daily-walk/core/cognito";
+import { LoginAttempts } from "@daily-walk/core/electrodb";
 import { baseProcedure, createTRPCRouter } from "../init";
 
 export const authRouter = createTRPCRouter({
-  signIn: baseProcedure.mutation((opts) => {
-    return true;
-  }),
+  signIn: baseProcedure
+    .input(
+      z.object({
+        username: z.string(),
+        password: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      console.info("auth_SignIn() :: Signing In");
+
+      const loginAttempt = await LoginAttempts.get({
+        username: input.username,
+      }).go({ attributes: ["attempts"] });
+
+      if (!!loginAttempt.data?.attempts && loginAttempt.data.attempts >= 3)
+        throw new Error("Max Attempt reached");
+
+      const result = await signInUser(input.username, input.password);
+
+      if (result.ChallengeName === "NEW_PASSWORD_REQUIRED" && !!result.Session)
+        return {
+          changePassword: true,
+          session: result.Session,
+        };
+
+      if (
+        result.AuthenticationResult?.AccessToken &&
+        result.AuthenticationResult.RefreshToken
+      ) {
+        const cookieExpiresAt = dayjs().add(12, "hour").toDate();
+
+        ctx.setCookie(
+          "app_accessToken",
+          result.AuthenticationResult.AccessToken,
+          {
+            httpOnly: true,
+            path: "/",
+            expires: cookieExpiresAt,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "lax",
+          }
+        );
+
+        ctx.setCookie(
+          "app_refreshToken",
+          result.AuthenticationResult.RefreshToken,
+          {
+            httpOnly: true,
+            path: "/",
+            expires: cookieExpiresAt,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "lax",
+          }
+        );
+
+        ctx.setCookie("app_refreshTokenExp", cookieExpiresAt.toString(), {
+          httpOnly: true,
+          path: "/",
+          expires: cookieExpiresAt,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "lax",
+        });
+      }
+
+      if (!!loginAttempt.data)
+        await LoginAttempts.delete({ username: input.username }).go();
+
+      console.info("auth_SignIn() :: Signing In Successful");
+
+      return {
+        success: true,
+      };
+    }),
   signOut: baseProcedure.mutation((opts) => {
     return true;
   }),

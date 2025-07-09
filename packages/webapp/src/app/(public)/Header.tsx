7cd0810de3f24ae851c715bc1654419982d08aec
marcotheo <marcobutalid989@gmail.com>
2025-07-09 11:06:00 +0800

    header responsiveness

--- packages/webapp/src/app/(public)/Header.tsx
+++ packages/webapp/src/app/(public)/Header.tsx
@@ -1,27 +1,49 @@
 import * as React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { MenuIcon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { DarkModeToggle } from "@/components/dark-mode-toggle";
+import { Button } from "@/components/ui/button";
 
 const HeaderLogo = () => {
   return (
-    <div className="">
-      <Image
-        src="/logo-dark-mode.png"
-        alt="Daily Walk Logo dark"
-        width={120}
-        height={120}
-        className="hidden dark:block"
-      />
-      <Image
-        src="/logo-light-mode.png"
-        alt="Daily Walk Logo light"
-        width={120}
-        height={120}
-        className="block dark:hidden"
-      />
-    </div>
+    <>
+      <div className="max-md:hidden">
+        <Image
+          src="/logo-dark-mode.png"
+          alt="Daily Walk Logo dark"
+          width={120}
+          height={120}
+          className="hidden dark:block"
+        />
+        <Image
+          src="/logo-light-mode.png"
+          alt="Daily Walk Logo light"
+          width={120}
+          height={120}
+          className="block dark:hidden"
+        />
+      </div>
+
+      <div className="md:hidden">
+        <Image
+          src="/logo-dark-mode.png"
+          alt="Daily Walk Logo dark"
+          width={100}
+          height={100}
+          className="hidden dark:block"
+        />
+        <Image
+          src="/logo-light-mode.png"
+          alt="Daily Walk Logo light"
+          width={100}
+          height={100}
+          className="block dark:hidden"
+        />
+      </div>
+    </>
   );
 };
 
@@ -29,13 +51,41 @@ export default function Header() {
   return (
     <div
       className={cn(
-        "px-5",
+        "px-5 md:px-16 xl:px-24",
         "flex justify-between items-center",
         "bg-transparent"
       )}
     >
       <HeaderLogo />
-      <div>
+
+      <div className={cn("space-x-0 md:space-x-2", "flex items-end")}>
+        <Button variant="ghost" size="icon" className="p-[1.6rem] lg:hidden">
+          <MenuIcon className={cn("size-7")} />
+        </Button>
+
+        <div className={cn("space-x-2", "flex items-end", "max-lg:hidden")}>
+          <Link href="/login">
+            <Button
+              variant="outline"
+              className={cn("h-full py-4 min-w-24", "rounded-3xl")}
+            >
+              Log in
+            </Button>
+          </Link>
+
+          <Link href="/login">
+            <Button
+              className={cn(
+                "h-full py-4 min-w-24",
+                "rounded-3xl",
+                "bg-secondary text-secondary-foreground hover:bg-secondary/90"
+              )}
+            >
+              Sign Up
+            </Button>
+          </Link>
+        </div>
+
         <DarkModeToggle />
       </div>
     </div>

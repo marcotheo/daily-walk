564798b80a28a941346976e5cb37417ae3211797
marcotheo <marcobutalid989@gmail.com>
2025-07-09 16:09:38 +0800

    separated header logo

--- packages/webapp/src/app/(public)/_components/layout/header.tsx
+++ packages/webapp/src/app/(public)/_components/layout/header.tsx
@@ -1,51 +1,11 @@
 import * as React from "react";
-import Image from "next/image";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 import { DarkModeToggle } from "@/components/dark-mode-toggle";
 import { Button } from "@/components/ui/button";
 import { SideNav } from "./sidenav";
-
-const HeaderLogo = () => {
-  return (
-    <>
-      <div className="max-md:hidden">
-        <Image
-          src="/logo-dark-mode.png"
-          alt="Daily Walk Logo dark"
-          width={120}
-          height={120}
-          className="hidden dark:block"
-        />
-        <Image
-          src="/logo-light-mode.png"
-          alt="Daily Walk Logo light"
-          width={120}
-          height={120}
-          className="block dark:hidden"
-        />
-      </div>
-
-      <div className="md:hidden">
-        <Image
-          src="/logo-dark-mode.png"
-          alt="Daily Walk Logo dark"
-          width={100}
-          height={100}
-          className="hidden dark:block"
-        />
-        <Image
-          src="/logo-light-mode.png"
-          alt="Daily Walk Logo light"
-          width={100}
-          height={100}
-          className="block dark:hidden"
-        />
-      </div>
-    </>
-  );
-};
+import HeaderLogo from "./headerlogo";
 
 export default function Header() {
   return (
---
+++ packages/webapp/src/app/(public)/_components/layout/headerlogo.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+export default function HeaderLogo() {
+  return (
+    <>
+      <div className="max-md:hidden">
+        <Image
+          src="/logo-dark-mode.png"
+          alt="Daily Walk Logo dark"
+          width={120}
+          height={120}
+          className="hidden dark:block"
+        />
+        <Image
+          src="/logo-light-mode.png"
+          alt="Daily Walk Logo light"
+          width={120}
+          height={120}
+          className="block dark:hidden"
+        />
+      </div>
+
+      <div className="md:hidden">
+        <Image
+          src="/logo-dark-mode.png"
+          alt="Daily Walk Logo dark"
+          width={100}
+          height={100}
+          className="hidden dark:block"
+        />
+        <Image
+          src="/logo-light-mode.png"
+          alt="Daily Walk Logo light"
+          width={100}
+          height={100}
+          className="block dark:hidden"
+        />
+      </div>
+    </>
+  );
+}

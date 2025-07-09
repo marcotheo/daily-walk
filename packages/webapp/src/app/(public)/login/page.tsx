2084e89ef937ffff29ff35a12d49b6ea8adf5ab8
marcotheo <marcobutalid989@gmail.com>
2025-07-09 15:52:26 +0800

    initialized login page

---
+++ packages/webapp/src/app/(public)/login/page.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function Login() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      This is login
+    </div>
+  );
+}

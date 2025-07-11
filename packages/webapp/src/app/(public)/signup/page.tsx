eeb56d3323083fcf2952470cc2215c3604238dc8
marcotheo <marcobutalid989@gmail.com>
2025-07-11 21:11:03 +0800

    initialized signup page

---
+++ packages/webapp/src/app/(public)/signup/page.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function Page() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      This is signup
+    </div>
+  );
+}

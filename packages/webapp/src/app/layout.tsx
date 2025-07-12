49f2982b9fc658185ce6781dd6a6a2c8b78781b1
marcotheo <marcobutalid989@gmail.com>
2025-07-12 13:26:38 +0800

    added toaster

--- packages/webapp/src/app/layout.tsx
+++ packages/webapp/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
         >
           <Providers>{children}</Providers>
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );

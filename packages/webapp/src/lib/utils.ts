0254aa684339d74655b70352acc4c92dd49b6fc7
marcotheo <marcobutalid989@gmail.com>
2025-07-12 11:35:36 +0800

    added regex validations;

--- packages/webapp/src/lib/utils.ts
+++ packages/webapp/src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const RegexValidations = {
+  NumberOnly: /(^[0-9]+$|^$)/,
+  NoNumber: /(^[a-zA-Z\s-'.]*$)/,
+  MobileNumberNoZero: /^9\d{2}-\d{3}-\d{4}$/,
+  Address: /(^[a-zA-Z0-9\s-'#.]*$)/,
+  MobileNumber: /^[0-9-]*$/,
+  hasNumber: /[0-9]/,
+  acceptedNumberWithNine: /\b9+/,
+  acceptedNumberWithZero: /\b09+/,
+  mobileNumberReg: /^(09|\+639)\d{9}$/,
+  hasLowerCase: /[a-z]/,
+  hasUpperCase: /[A-Z]/,
+  hasSpecialChar: /[`!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/,
+};

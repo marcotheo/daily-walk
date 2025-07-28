899f29c344b2576b95ce4e02ea72ee0f6b20d501
marcotheo <marcobutalid989@gmail.com>
2025-07-28 16:06:57 +0800

    added login form

--- packages/webapp/src/app/(auth)/login/_components/login-form.tsx
+++ packages/webapp/src/app/(auth)/login/_components/login-form.tsx
@@ -1,7 +1,108 @@
+"use client";
+
 import * as React from "react";
+import { useForm } from "react-hook-form";
+import * as v from "valibot";
+import { toast } from "sonner";
+import { valibotResolver } from "@hookform/resolvers/valibot";
+import { useRouter } from "next/navigation";
 
-export interface ILoginFormProps {}
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { trpc } from "@/trpc/client";
+import { LoadingOverlay } from "@/components/LoadingOverlay";
 
-export default function LoginForm(props: ILoginFormProps) {
-  return <div>this is the form</div>;
+const schema = v.pipe(
+  v.object({
+    email: v.pipe(
+      v.string(),
+      v.nonEmpty("Please enter your email."),
+      v.email("Invalid Email Address.")
+    ),
+    password: v.pipe(v.string(), v.nonEmpty("Please enter your password.")),
+  })
+);
+
+type FormInputs = v.InferInput<typeof schema>;
+
+export default function SignUpForm() {
+  const router = useRouter();
+  const userLogin = trpc.auth.signIn.useMutation();
+
+  const form = useForm<FormInputs>({
+    resolver: valibotResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: FormInputs) => {
+    userLogin.mutate(
+      {
+        email: data.email,
+        password: data.password,
+      },
+      {
+        onError: (error) => {
+          toast.error(error.message);
+        },
+        onSuccess: () => {
+          form.reset();
+          userLogin.reset();
+          router.push("/");
+          // setLogin(true);
+        },
+      }
+    );
+  };
+
+  return (
+    <div>
+      <LoadingOverlay open={userLogin.isPending} message="Authenticating" />
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className="space-y-5">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input type="password" placeholder="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <Button className="w-full rounded-sm" size="lg">
+            LOG IN
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
 }
--- packages/webapp/src/app/(auth)/login/page.tsx
+++ packages/webapp/src/app/(auth)/login/page.tsx
@@ -1,9 +1,55 @@
 import * as React from "react";
+import Link from "next/link";
 
-export default function Login() {
+import { cn } from "@/lib/utils";
+import LoginForm from "./_components/login-form";
+import { DarkModeToggle } from "@/components/dark-mode-toggle";
+import HeaderLogo from "@/app/(public)/_components/layout/header-logo";
+
+export default function Page() {
   return (
-    <div className="h-screen w-full flex justify-center items-center">
-      This is login
+    <div className="h-screen w-full flex">
+      <div className={cn("w-2/3 py-5 pl-5", "max-lg:hidden")}>
+        <div
+          className={cn("w-full h-full", "rounded-3xl", "bg-gray-400")}
+        ></div>
+      </div>
+
+      <div
+        className={cn(
+          "relative",
+          "w-full lg:w-1/3",
+          "flex justify-center items-center"
+        )}
+      >
+        <div
+          className={cn(
+            "w-full absolute top-0",
+            "flex justify-between items-center",
+            "px-5 xl:px-16"
+          )}
+        >
+          <HeaderLogo />
+
+          <DarkModeToggle />
+        </div>
+
+        <div className="space-y-10 w-full px-5 xl:px-16">
+          <div className="space-y-1">
+            <h1 className="text-2xl md:text-3xl">Sign In</h1>
+
+            <div className="flex gap-1 text-sm">
+              <p className="text-gray-500">Don't have an account?</p>
+              <Link href="/login">
+                <p className="underline text-info">Sign up here</p>
+              </Link>
+              .
+            </div>
+          </div>
+
+          <LoginForm />
+        </div>
+      </div>
     </div>
   );
 }
